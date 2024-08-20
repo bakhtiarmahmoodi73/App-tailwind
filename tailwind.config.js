1f06d5f473a9be8df610c751c98847eb24374f10
bakhtiarmahmoodi73 <bakhtiarmahmoodi1995@gmail.com>
2024-08-20 13:38:57 -0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        // res_header: { max: "486px" },
+        // res_nav: {max:"1026px"},
+        // res_banner: {max:"500px"}
+      },
+      colors: {
+        bg_header: "#E2E2E2",
+        bg_banner: "#333333",
+      },
       fontFamily: {
-        IRANSansX: "IRANSansX",
+        fonts: ["Georgia", "Times New Roman", "Times", "serif"],
       },
     },
   },
