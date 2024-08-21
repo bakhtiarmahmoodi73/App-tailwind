e13f1b4ccbdfe0e90a724b379020ca4ece939f52
bakhtiarmahmoodi73 <bakhtiarmahmoodi1995@gmail.com>
2024-08-21 09:41:41 -0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         bg_header: "#E2E2E2",
         bg_banner: "#333333",
         bg_nav: "#404040",
+        bg_img: "#FFFFFF",
       },
       fontFamily: {
         fonts: ["Georgia", "Times New Roman", "Times", "serif"],
